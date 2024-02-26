71250cfe54fd1fadb727397383476c55b2a12987
martin <vazquezmartin1240@gmail.com>
2024-02-25 20:21:31 -0600

    se agrego la bd de railway

--- SecretariaInovacion/settings.py
+++ SecretariaInovacion/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,10 +86,10 @@ WSGI_APPLICATION = 'SecretariaInovacion.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Fafba6Fc5-e4b2ca*dA-daGfe121eab2@roundhouse.proxy.rlwy.net:53270/railway',
+        conn_max_age=600
+    )
 }
 
 
